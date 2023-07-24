d580db0bea70ec8586b485a7cc71899a3a2132d7
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-24 13:28:45 +0200

    Fix module resolving issues with tests

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { AttackType, getAttack } from "./attack/AttackType";
-import Player from "./player/Player";
+import { AttackType } from "@attack/AttackType";
+import Player from "@player/Player";
 
 const at = AttackType.Axe;
 const p = new Player(25, 16);
@@ -8,4 +8,3 @@ for (let i = 0; i < 10; ++i) {
   const r = p.takeHit(at);
   console.log(r);
 }
-export { Player };
