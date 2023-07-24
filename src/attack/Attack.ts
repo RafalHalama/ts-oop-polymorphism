383e34ad427335c1b51b16b4456ab56020cf3e02
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-24 11:45:12 +0200

    Complete core exercise

---
+++ src/attack/Attack.ts
@@ -0,0 +1,22 @@
+import throwDice from "../dice/Dice";
+
+class Attack {
+    constructor(
+        private _name: string,
+        private _accuracyRoll: 20,
+        private _damageRange: [number, number]
+    ) {}
+
+    get name(): string {
+        return this._name
+    }
+
+    public rollForAccuracy = () => throwDice(this._accuracyRoll)
+
+    public rollForDamage = () => {
+        const [min, max] = this._damageRange
+        return Math.round(Math.random() * (max - min) + min)
+    }
+}
+
+export default Attack
